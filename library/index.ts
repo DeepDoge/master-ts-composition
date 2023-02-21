d4ca2643ec2f50013924b7f3f678d727a1f360e4
Shiba <44804845+DeepDoge@users.noreply.github.com>
2023-02-21 21:27:06 +0300

    ...

--- library/index.ts
+++ library/index.ts
@@ -1,9 +1,14 @@
 type Obj = Record<string, any>
 
+type Intersector<T extends Obj> = {
+	intersect<U extends Obj>(other: InstanceableType<U>): Intersector<T & U>
+	$(): InstanceableType<T>
+}
+
 export type InstanceableType<T extends Obj> = {
 	" __is_intersection__ ": boolean
 	" __intersections__ ": Set<InstanceableType<any>>
-	intersect<U extends Obj>(other: InstanceableType<U>): InstanceableType<T & U>
+	intersect<U extends Obj>(other: InstanceableType<U>): Intersector<T & U>
 	new (init: T): T
 }
 
@@ -21,11 +26,24 @@ export function instanceableType<This extends Obj = {}>() {
 		static intersect<Other extends Obj>(other: InstanceableType<Other>) {
 			const NEW = instanceableType<This & Other>()
 			NEW[" __is_intersection__ "] = true
-			NEW[" __intersections__ "].add(this)
-			this[" __intersections__ "].forEach((intersection) => NEW[" __intersections__ "].add(intersection))
-			NEW[" __intersections__ "].add(other)
-			other[" __intersections__ "].forEach((intersection) => NEW[" __intersections__ "].add(intersection))
-			return NEW
+			const intersectionsOfNEW = NEW[" __intersections__ "]
+			intersectionsOfNEW.add(this)
+			this[" __intersections__ "].forEach((intersection) => intersectionsOfNEW.add(intersection))
+			intersectionsOfNEW.add(other)
+			other[" __intersections__ "].forEach((intersection) => intersectionsOfNEW.add(intersection))
+
+			const intersector = {
+				intersect<U extends Obj>(other: InstanceableType<U>) {
+					intersectionsOfNEW.add(other)
+					other[" __intersections__ "].forEach((intersection) => intersectionsOfNEW.add(intersection))
+					return intersector
+				},
+				$() {
+					return NEW
+				},
+			}
+
+			return intersector as Intersector<This & Other>
 		}
 
 		constructor(init: This) {
